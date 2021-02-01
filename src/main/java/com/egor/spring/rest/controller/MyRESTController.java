455bfa1bd83068abd3ca9f4b6048f8944b3bd20b
Egor Anchutin <zbit16@gmail.com>
2021-02-01 23:31:00 +0300

    Add exception handler

--- src/main/java/com/egor/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/egor/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.egor.spring.rest.controller;
 
 import com.egor.spring.rest.entity.Employee;
+import com.egor.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.egor.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.egor.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +31,27 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in database");
+        }
+
         return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handlerException(NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handlerException(Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
